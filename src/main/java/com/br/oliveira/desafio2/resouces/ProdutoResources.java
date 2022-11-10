82b8d963c3d413f7697c1314444c68da06ab876e
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-11-10 18:57:27 -0300

    início do projeto com primeira entidade e configuração do banco H2

---
+++ src/main/java/com/br/oliveira/desafio2/resouces/ProdutoResources.java
@@ -0,0 +1,9 @@
+package com.br.oliveira.desafio2.resouces;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProdutoResources {
+
+
+}
---
+++ src/main/java/com/br/oliveira/desafio2/service/ProdutoServiceImplement.java
@@ -0,0 +1,7 @@
+package com.br.oliveira.desafio2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoServiceImplement implements ProdutoService{
+}

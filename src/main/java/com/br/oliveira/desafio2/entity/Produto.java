9c6cf231c98a5aebfa6d46e52caccb77131aa6b7
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-11-11 09:21:07 -0300

    Adição do Lombok e Builder pattern ao projeto

--- src/main/java/com/br/oliveira/desafio2/entity/Produto.java
+++ src/main/java/com/br/oliveira/desafio2/entity/Produto.java
@@ -1,9 +1,18 @@
 package com.br.oliveira.desafio2.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Produto {
 
     @Id
@@ -15,65 +24,5 @@ public class Produto {
     @Enumerated(EnumType.STRING)
     private ProdutoCategoria produtoCategoria;
 
-    public Long getProdutoId() {
-        return produtoId;
-    }
 
-    public void setProdutoId(Long produtoId) {
-        this.produtoId = produtoId;
-    }
-
-    public String getProdutoNome() {
-        return produtoNome;
-    }
-
-    public void setProdutoNome(String produtoNome) {
-        this.produtoNome = produtoNome;
-    }
-
-    public BigDecimal getProdutoPreco() {
-        return produtoPreco;
-    }
-
-    public void setProdutoPreco(BigDecimal produtoPreco) {
-        this.produtoPreco = produtoPreco;
-    }
-
-    public int getProdutoQuantidade() {
-        return produtoQuantidade;
-    }
-
-    public void setProdutoQuantidade(int produtoQuantidade) {
-        this.produtoQuantidade = produtoQuantidade;
-    }
-
-    public ProdutoCategoria getProdutoCategoria() {
-        return produtoCategoria;
-    }
-
-    public void setProdutoCategoria(ProdutoCategoria produtoCategoria) {
-        this.produtoCategoria = produtoCategoria;
-    }
-
-    public Produto(Long produtoId, String produtoNome, BigDecimal produtoPreco, int produtoQuantidade, ProdutoCategoria produtoCategoria) {
-        this.produtoId = produtoId;
-        this.produtoNome = produtoNome;
-        this.produtoPreco = produtoPreco;
-        this.produtoQuantidade = produtoQuantidade;
-        this.produtoCategoria = produtoCategoria;
-    }
-
-    public Produto() {
-    }
-
-    @Override
-    public String toString() {
-        return "Produto{" +
-                "produtoId=" + produtoId +
-                ", produtoNome='" + produtoNome + '\'' +
-                ", produtoPreco=" + produtoPreco +
-                ", produtoQuantidade=" + produtoQuantidade +
-                ", produtoCategoria=" + produtoCategoria +
-                '}';
-    }
 }

cd26756dec1a627f543653018dc0ecc7391aaa38
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2023-01-11 16:45:28 -0300

    configuração do RabbitMQ com Docker e criação das filas

---
+++ src/main/java/com/br/oliveira/constantes/RabbitMQConstantes.java
@@ -0,0 +1,8 @@
+package com.br.oliveira.constantes;
+
+public class RabbitMQConstantes {
+
+    public static final String FILA_ESTOQUE = "ESTOQUE";
+    public static final String FILA_PRECO = "PRECO";
+
+}
